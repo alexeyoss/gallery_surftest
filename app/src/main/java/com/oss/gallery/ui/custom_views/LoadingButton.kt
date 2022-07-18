0dc2f3a9c6428ccf260bd8002252de9f7a3b6324
alexey_oss <mukovozov19281@gmail.com>
2022-07-18 09:11:45 +0300

    added LoadingButton.kt

---
+++ app/src/main/java/com/oss/gallery/ui/custom_views/LoadingButton.kt
@@ -0,0 +1,37 @@
+package com.oss.gallery.ui.custom_views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.withStyledAttributes
+import androidx.core.view.isVisible
+import com.oss.gallery.R
+import com.oss.gallery.databinding.LoadingButtonBinding
+
+class LoadingButton @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet?
+) : ConstraintLayout(context, attrs) {
+
+    private val binding = LoadingButtonBinding.inflate(LayoutInflater.from(context), this)
+
+    private var buttonText: String = ""
+        set(value) {
+            field = value
+            binding.button.text = value
+        }
+
+    private var loading: Boolean = false
+        set(value) {
+            field = value
+            binding.progressBar.isVisible = value
+        }
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton){
+            buttonText = getString(R.styleable.LoadingButton_lb_text) ?: ""
+            loading = getBoolean(R.styleable.LoadingButton_lb_loading, false)
+        }
+    }
+}
